b42b3f17acf32f6f62a9c5ad1d5827e1e47585ce
Andrew Gazeley <Andrew Gazeley@DESKTOP-1VR32OA>
2016-11-13 22:42:03 -0800

    Added basic winforms app and attempts at sql server scripts for when we have a server

---
+++ TheatreManagerApp/TheatreManagerApp/dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Staff]
+(
+	[employee_ssn] INT NOT NULL PRIMARY KEY, 
+    [fname] NCHAR(30) NULL, 
+    [lname] NCHAR(30) NULL, 
+    [type_id] INT NULL
+)
---
+++ TheatreManagerApp/TheatreManagerApp/dbo.Type.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Type]
+(
+	[type_id] INT NOT NULL PRIMARY KEY, 
+    [staff_type] NCHAR(20) NULL, 
+    [salary] MONEY NULL
+)
